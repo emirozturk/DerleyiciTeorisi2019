e7084278a76bf9d45efc6cb4a09f968f541678c0
Emir Öztürk <emirozturk3@gmail.com>
2019-04-15 14:01:09 +0300

    Added AST and Parser Prototypes

---
+++ DerleyiciTeorisi2019/AST.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerleyiciTeorisi2019
+{
+    class Durum { }
+    class Ifade { }
+    class Blok : Durum
+    {
+        public List<Durum> durumListesi { get; set; }
+        public Blok()
+        {
+            durumListesi = new List<Durum>();
+        }
+    }
+    class Fonksiyon : Blok
+    {
+        string ad;
+        List<string> parametreler;
+        public Fonksiyon(string ad, List<string> parametreler)
+        {
+            this.ad = ad;
+            this.parametreler = parametreler;
+        }
+    }
+    class Atama : Durum
+    {
+        string ad;
+        Ifade deger;
+        public Atama(string ad, Ifade deger)
+        {
+            this.ad = ad;
+            this.deger = deger;
+        }
+    }
+    class Cagirma : Durum
+    {
+        string ad;
+        List<Ifade> parametreler;
+        public Cagirma(string ad, List<Ifade> parametreler)
+        {
+            this.ad = ad;
+            this.parametreler = parametreler;
+        }
+    }
+    class Tamsayi : Ifade
+    {
+        int deger;
+        public Tamsayi(int deger)
+        {
+            this.deger = deger;
+        }
+    }
+    class Tanim : Ifade
+    {
+        string deger;
+        public Tanim(string deger)
+        {
+            this.deger = deger;
+        }
+    }
+    class MatematikIfadesi : Ifade
+    {
+        Ifade solIfade;
+        TokenTuru islem;
+        Ifade sagIfade;
+        public MatematikIfadesi(Ifade solIfade, TokenTuru islem, Ifade sagIfade)
+        {
+            this.solIfade = solIfade;
+            this.islem = islem;
+            this.sagIfade = sagIfade;
+        }
+    }
+}
---
+++ DerleyiciTeorisi2019/Parser.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerleyiciTeorisi2019
+{
+    class Parser
+    {
+        private TokenListesi tl;
+        Blok suAnkiBlok = null;
+        Stack<Blok> blokStack = new Stack<Blok>();
+        List<Durum> agac = new List<Durum>();
+        List<string> eklentiler = new List<string>();
+        public Parser(TokenListesi tl)
+        {
+            this.tl = tl;
+            parseEt();
+        }
+
+        private void parseEt()
+        {
+            while (true)
+            {
+                Token t = tl.TokenAl();
+                if (t == null) break;
+                if (t.Tur == TokenTuru.Fonk)
+                {
+                    suAnkiBlok = fonksiyonParseEt();
+                }
+                else if (t.Tur == TokenTuru.Tanim)
+                {
+                    if (tl.Gozat().Tur == TokenTuru.Esittir)
+                    {
+                        tl.GeriGit();
+                        Atama a = atamaParseEt();
+                        suAnkiBlok.durumListesi.Add(a);
+                    }
+                    else if (tl.Gozat().Tur == TokenTuru.ParantezAc)
+                    {
+                        tl.GeriGit();
+                        Cagirma c = cagirmaParseEt();
+                        suAnkiBlok.durumListesi.Add(c);
+                    }
+                }
+            }
+        }
+
+        private Cagirma cagirmaParseEt()
+        {
+            string ad = tl.TokenAl().Deger;
+            tl.IleriGit();
+            List<Ifade> argumanlar = new List<Ifade>();
+            if (tl.Gozat().Tur == TokenTuru.ParantezKapat)
+                argumanlar = argumanParseEt();
+            return new Cagirma(ad, argumanlar);
+        }
+
+        private List<Ifade> argumanParseEt()
+        {
+            List<Ifade> argumanlar = new List<Ifade>();
+            while (true)
+            {
+                argumanlar.Add(ifadeParseEt());
+                if (tl.Gozat().Tur == TokenTuru.ParantezKapat)
+                {
+                    tl.IleriGit();
+                    break;
+                }
+            }
+            return argumanlar;
+        }
+
+        private Atama atamaParseEt()
+        {
+            string ad = tl.TokenAl().Deger;
+            tl.IleriGit();
+            Ifade ifade = ifadeParseEt();
+            return new Atama(ad, ifade);
+        }
+
+        private Ifade ifadeParseEt()
+        {
+            Ifade donus = null;
+            Token t = tl.TokenAl();
+            if (t.Tur == TokenTuru.TamSayi)
+                donus = new Tamsayi(Convert.ToInt32(t.Deger));
+            else if (t.Tur == TokenTuru.Tanim)
+                donus = new Tanim(t.Deger);
+            if (tl.Gozat().Tur == TokenTuru.Arti)
+            {
+                Ifade solIfade = donus;
+                TokenTuru islem = tl.TokenAl().Tur;
+                Ifade sagIfade = ifadeParseEt();
+                donus = new MatematikIfadesi(solIfade, islem, sagIfade);
+            }
+            return donus;
+        }
+
+        private Fonksiyon fonksiyonParseEt()
+        {
+            string ad = tl.TokenAl().Deger;
+            tl.IleriGit();
+            List<string> parametreler = new List<string>();
+            if (tl.Gozat().Tur == TokenTuru.ParantezKapat)
+                tl.IleriGit();
+            else
+                parametreler = parametreParseEt();
+            return new Fonksiyon(ad, parametreler);
+        }
+
+        private List<string> parametreParseEt()
+        {
+            List<string> parametreler = new List<string>();
+            while (true)
+            {
+                Token t = tl.TokenAl();
+                if (t.Tur == TokenTuru.Tanim)
+                    parametreler.Add(t.Deger);
+                if (tl.Gozat().Tur == TokenTuru.ParantezKapat)
+                {
+                    tl.IleriGit();
+                    break;
+                }
+            }
+            return parametreler;
+        }
+    }
+}
